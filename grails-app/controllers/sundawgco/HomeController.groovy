7a8b6b7b4ae2daf94122be26929834d0db6d32a7
Christopher Sun <christopher.sun@yahoo.com>
2010-10-12 11:58:24 -0400

    upgraded client jar, fixed failed rendering handling

--- grails-app/controllers/sundawgco/HomeController.groovy
+++ grails-app/controllers/sundawgco/HomeController.groovy
@@ -99,6 +99,9 @@ class HomeController {
       apiClient.reload(video)
       render(text: "{\"completed\":true, \"url\":\"" + request.contextPath + "/play?links[file]=" + URLEncoder.encode(video.links.get("file"))+ "\"}", contentType: "text/json")
     }
+    else if (renderingJob.failed) {
+      throw new Exception("Rendering job failed [" + renderingJob.metadata + "])
+    }
     else {
       render(text: "{\"completed\":false}", contentType:"text/json")
     }
